d85787497e42eb49349720d24d78fcdef24c78e1
DeVector <diniz.vito@gmail.com>
2025-03-29 17:28:50 -0300

    Criando os modelos para utilizar no projeto

---
+++ src/models/Emergencia.ts
@@ -0,0 +1,14 @@
+class Emergencia {
+  nome: string;
+  numero: number;
+  id: number;
+
+  constructor(nome: string, numero: number, id: number){
+    this.id = id;
+    this.nome = nome;
+    this.numero = numero;
+  }
+
+}
+
+export default Emergencia;
