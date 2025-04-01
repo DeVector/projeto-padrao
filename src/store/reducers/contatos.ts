2042d07e0a3af1fa4d5d46b8b946e3ba826a976a
DeVector <diniz.vito@gmail.com>
2025-04-01 17:34:13 -0300

    Criando reducer de cadastrar

--- src/store/reducers/contatos.ts
+++ src/store/reducers/contatos.ts
@@ -49,7 +49,31 @@ const contatoSlice = createSlice({
         (contato) => contato.id !== action.payload
       )
     },
-    
+    editar: (state, action: PayloadAction<Contato>) => {
+      const indexDoContato = state.itens.findIndex(
+        (c) => c.id === action.payload.id
+      )
+
+      if (indexDoContato >= 0) {
+        state.itens[indexDoContato] = action.payload;
+      }
+    },
+    cadastrar: (state, action: PayloadAction<Omit<Contato, 'id'>>) => {
+      const contatoJaExiste = state.itens.find(
+        (c) => c.email.toLocaleLowerCase() === action.payload.email.toLocaleLowerCase() && c.telefone === action.payload.telefone
+      )
+
+      if (contatoJaExiste) {
+        alert('Contato ja existente na lista')
+      } else {
+        const ultimoContato = state.itens[state.itens.length - 1]
+        const novoContato = {
+          ...action.payload,
+          id: ultimoContato ? ultimoContato.id + 1 : 1
+        }
+        state.itens.push(novoContato)
+      }
+    }
   }
 })
 
